b1c3f2da24b9c62e9c7452e5af10a85c237ff1ab
Eduardo Rosselot <earosselot@gmail.com>
2022-07-21 18:50:47 -0300

    andandoo

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react'
 
 function App() {
+
+  const [number, setNumber] = useState()
+
+  // const [time, setTime] = useState(Date.now());
+
+  function newNumber() {
+    const newNumber = Math.floor(Math.random() * 100)
+    console.log(newNumber)
+    setNumber(newNumber)
+  }
+
+  useEffect(() => {
+    const interval = setInterval(() => newNumber(), 4000)
+    return () => {
+      newNumber()
+      clearInterval(interval)
+    };
+  }, [])
+
+
+  function handleClick(e) {
+    e.preventDefault()
+    newNumber()
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>{number}</h1>
+        <button onClick={handleClick}>SAPE</button>
       </header>
     </div>
   );
